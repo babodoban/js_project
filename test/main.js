43a60513cdfe6fec357f46374a87c5a2e6a11463
Sean <babodoban@gmail.com>
2018-12-10 23:11:51 +0900

    #1-7. let const var, we need to use let

--- test/main.js
+++ test/main.js
@@ -7,7 +7,14 @@ console.log('Im working. Im JS. Im Beautiful. Im worth it');
 
 // create - initialize - use
 
-let a = 221;
-let b = a - 5;
-a = 4;
-console.log(b, a);
+// let a = 221;
+// let b = a - 5;
+// a = 4;
+// console.log(b, a);
+
+const a = 221; //constant 상수. 변하지 않는 값
+//let 과 const 의 차이점. let은 변수를 바꿀 수 있고, const 는 바꿀 수 없는 값. 
+//let const var 
+//let으로 변수를 create 하고 initialize, 그리고 하나의 변수에 한 번만 선언 가능. 중복으로 변수 선언하면 에러가 발생
+//const는 변수가 아니라 고정된 값, 상수를 선언하는 것
+//var는 변수를 선언하지만 하나의 변수에 여러번 선언 가능. 
